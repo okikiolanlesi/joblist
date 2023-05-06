e4b13f030a9bf8863b3f1eceb4da3a06eb7d03ad
okikiolanlesi <osunronbiolanlesi@gmail.com>
2023-05-06 10:59:16 +0100

    basic setup

---
+++ app/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Stack } from "expo-router";
+import { View, Text } from "react-native";
+
+const index = () => {
+  return (
+    <View>
+      <Text>Okikiola</Text>
+    </View>
+  );
+};
+
+export default index;
